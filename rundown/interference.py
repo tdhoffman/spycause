9bd875b1e9b46ea89b8de26a1ff495fd6624f234
thoffman1 <tylerhoff1@gmail.com>
2022-11-14 10:28:29 -0700

    interference adjustment completed

--- rundown/interference.py
+++ rundown/interference.py
@@ -7,7 +7,24 @@ This should be a transformer!!!!
 """
 
 import numpy as np
+from libpysal.weights import W as WeightsType
+from sklearn.base import BaseEstimator, TransformerMixin
 
-class InterferenceAdjuster:
-    def __init__(self, Z, w):
-        Z = np.hstack((w @ Z))
+class InterferenceAdj(BaseEstimator, TransformerMixin):
+    def __init__(self, w=None):
+        self.w = w
+
+    def fit(self):
+        return self
+
+    def transform(self, Z):
+        # Input checks
+        if len(Z.shape) < 2:  # ensure column vector
+            Z = Z.reshape(-1, 1)
+
+        if type(self.w) == WeightsType:
+            weights = self.w.full()[0]
+        else:
+            weights = self.w
+
+        return np.hstack((Z, np.dot(weights, Z)))
