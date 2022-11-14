39f95615d7da53ccb17a8c906fcc7c435a5ca10c
thoffman1 <tylerhoff1@gmail.com>
2022-11-14 10:36:08 -0700

    formatting and fixing `fit` method for `InterferenceAdj`

--- rundown/interference.py
+++ rundown/interference.py
@@ -3,28 +3,32 @@ __author__ = "Tyler D. Hoffman cause@tdhoffman.com"
 """
 Spatial interference adjustments for causal inference.
 Basically just include a lag of the treatment variables
-This should be a transformer!!!!
 """
 
 import numpy as np
 from libpysal.weights import W as WeightsType
 from sklearn.base import BaseEstimator, TransformerMixin
 
+
 class InterferenceAdj(BaseEstimator, TransformerMixin):
+    """
+    Spatial lag transformer: adjoins a lag of the covariates
+    """
+
     def __init__(self, w=None):
         self.w = w
 
-    def fit(self):
-        return self
+    def fit(self, X, y=None):
+        return self  # nothing to fit
 
-    def transform(self, Z):
+    def transform(self, X, y=None):
         # Input checks
-        if len(Z.shape) < 2:  # ensure column vector
-            Z = Z.reshape(-1, 1)
+        if len(X.shape) < 2:  # ensure column vector
+            X = X.reshape(-1, 1)
 
         if type(self.w) == WeightsType:
             weights = self.w.full()[0]
         else:
             weights = self.w
 
-        return np.hstack((Z, np.dot(weights, Z)))
+        return np.hstack((X, np.dot(weights, X)))
