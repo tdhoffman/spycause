bb82bc274a86342828b0afc9620689220bec6f20
thoffman1 <tylerhoff1@gmail.com>
2023-03-21 11:14:56 -0700

    add spycause to path!

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sys, os
+sys.path.insert(0, os.path.abspath("../"))
+
 project = 'spycause'
 copyright = '2023, Tyler D. Hoffman'
 author = 'Tyler D. Hoffman'
