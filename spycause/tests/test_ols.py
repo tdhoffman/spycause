bd058a0b00772fd970ab8edc7210879b5be6951a
thoffman1 <tylerhoff1@gmail.com>
2023-04-07 15:05:19 -0700

    create pyproject and LICENSE

--- spycause/tests/test_ols.py
+++ spycause/tests/test_ols.py
@@ -1,1 +1,28 @@
 import unittest
+
+
+class TestOLS(unittest.TestCase):
+    def setUp(self):
+        # Create data
+
+    def tearDown(self):
+        pass
+
+    def test_fit(self):
+        pass
+
+    def test_fit_interf(self):
+        pass
+
+    def test_predict(self):
+        pass
+
+    def test_score(self):
+        pass
+
+    def test_waic(self):
+        pass
+
+    def test_diagnostics(self):
+        pass
+
